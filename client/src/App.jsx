93cb10917b7ed566fc36302deda29ece17de7205
Chernet <cherrefera@gmail.com>
2026-01-11 10:41:05 +0300

    feat: add Clerk auth, login, sidebar, and home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+
 import Login from './pages/Login'
 import Feed from './pages/Feed'
 import Messages from './pages/Messages'
@@ -8,24 +9,27 @@ import Connections from './pages/Connections'
 import Discover from './pages/Discover'
 import Profile from './pages/Profile'
 import CreatePost from './pages/CreatePost'
+import Layout from './pages/Layout'
+import {useUser} from "@clerk/clerk-react";
 const App = () => {
+  const {user} = useUser();
   return (
     <>
       <Routes>
         {/* We Define our routes here */}
-        <Route path="/" element={<Login/>}>
-          <Route index element={<Feed />}/>
-          <Route path='message' element={<Messages/>}/>
-          <Route path='message/:userId' element={<ChatBox />}/>
-          <Route path='connection' element={<Connections />}/>
-          <Route path='discover' element={<Discover />}/>
-          <Route path='profile' element={<Profile />}/>
-          <Route path='profile/:profileId' element={<Profile />}/>
-          <Route path='create-post' element={<CreatePost />}/>
+        <Route path="/" element={!user ? <Login /> : <Layout/>}>
+          <Route index element={<Feed />} />
+          <Route path="message" element={<Messages />} />
+          <Route path="message/:userId" element={<ChatBox />} />
+          <Route path="connection" element={<Connections />} />
+          <Route path="discover" element={<Discover />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="profile/:profileId" element={<Profile />} />
+          <Route path="create-post" element={<CreatePost />} />
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
 export default App
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import Sidebar from '../components/sidebar/SideBar'
+import { Outlet } from 'react-router-dom'
+import { Menu, X } from 'lucide-react';
+import { dummyUserData } from '../assets/assets';
+import Loading from '../components/loading/Loading';
 
 const Layout = () => {
-  return (
-    <div>Layout</div>
+  const user = dummyUserData
+  const [sideBarOpen, setSidebarOpen] = React.useState(false);
+  return user ? (
+    <div className="w-full h-screen flex">
+      <Sidebar />
+      <div className="flex-1 bg-slate-50">
+        <Outlet />
+      </div>
+      {sideBarOpen ? (
+        <X
+          className="absolute top-3 right-3 z-100 bg-white rounded-md shadow
+         w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => setSidebarOpen(false)}
+        />
+      ) : (
+        <Menu
+          className="absolute top-3 right-3 z-100 bg-white rounded-md shadow
+         w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => setSidebarOpen(true)}
+        />
+      )}
+    </div>
+  ) : (
+      <Loading/>
   )
 }
 
